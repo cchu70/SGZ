0ee7e00eecefab567e4d5ad3cff063c83c53a699
cchu70 <cchu@broadinstitute.org>
2023-07-25 03:40:37 +0000

    fix syntax

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='SGZ',
     version='0.1.0',
-    python_requires='==2.7.6'
+    python_requires='==2.7.6',
     packages=find_packages(),
     install_requires = [
         'pillow', 
